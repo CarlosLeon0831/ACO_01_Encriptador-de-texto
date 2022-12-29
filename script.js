d215385d85c870c1142bdbd2c69b47b8cbc11d91
Carlos <carlosleon0831@gmail.com>
2022-12-28 20:56:50 -0600

    Add pseudocode in comments to script file

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+    Pseudocode
+    Encrypt/Decript function:
+    Imput text
+    Click to either encrypt or decrypt
+    save input text
+    if (clicked on encrypt) {
+        look for vocal letters and change them to their respective keys
+    }
+    if (clicked on decrypt) {
+        look for encryption keys and change them to their respective letters
+    } 
+    return output text
+    show encrypted/decrypted text on output zone
+*/
