5073ac1e5ec4ad4274df44cf34726424908600fa
Carlos <carlosleon0831@gmail.com>
2022-12-30 17:19:37 -0600

    Start implementation of encrypt/dectypt functions

--- script.js
+++ script.js
@@ -13,3 +13,83 @@
     return output text
     show encrypted/decrypted text on output zone
 */
+const btnEncrypt = document.querySelector('#encrypt');
+const btnDecrypt = document.querySelector('#decrypt');
+
+btnEncrypt.addEventListener("click", encrypt);
+btnDecrypt.addEventListener("click", decrypt);
+
+encrypt = message => {
+    let encryptedMessage;
+
+    const messageArr = message.split("");
+    
+    let encryptedMessageArr = [];
+
+    for (i = 0; i < messageArr.length; i++) {
+        let letter;
+        switch(messageArr[i]) {
+            case "a":
+                letter = "ai";
+                break;
+            case "e":
+                letter = "enter";
+                break;
+            case "i":
+                letter = "imes";
+                break;
+            case "o":
+                letter = "ober";
+                break;
+            case "u":
+                letter = "ufat";
+                break;
+            default:
+                letter = messageArr[i];
+                break;
+        }
+
+        encryptedMessageArr.push(letter);
+    }
+
+    encryptedMessage = encryptedMessageArr.join("");
+    return encryptedMessageArr;
+};
+
+decrypt = message => {
+    let decryptedMessage;
+
+    const messageArr = message.split("");
+    
+    let decryptedMessageArr = [];
+
+    for (i = 0; i < messageArr.length; i++) {
+        let letter;
+        switch(messageArr[i]) {
+            case "ai":
+                letter = "a";
+                break;
+            case "enter":
+                letter = "e";
+                break;
+            case "imes":
+                letter = "i";
+                break;
+            case "ober":
+                letter = "o";
+                break;
+            case "ufat":
+                letter = "u";
+                break;
+            default:
+                letter = messageArr[i];
+                break;
+        }
+
+        decryptedMessageArr.push(letter);
+    }
+
+    decryptedMessage = decryptedMessageArr.join("");
+    return decryptedMessageArr;
+};
+
