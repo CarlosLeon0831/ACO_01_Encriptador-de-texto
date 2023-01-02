afc3be48478a87f86266275e6a06c6938af1bb49
Carlos <carlosleon0831@gmail.com>
2023-01-02 14:27:16 -0600

    Fix script and link to script in html file

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
+// Get the encrypt and decrypt buttons and the empty and full containers
 const btnEncrypt = document.getElementById('encrypt');
 const btnDecrypt = document.getElementById('decrypt');
 const emptyContainer = document.getElementById('empty');
 const fullContainer = document.getElementById('full');
 const output = document.getElementById('output');
 
+// Function to encrypt the string
 function encrypt(str) {
+    // Make the string lowercase
     str = str.toLowerCase();
+    // Replace certain letters with the encryption keys
     return str.replace(/e/g, "enter")
       .replace(/i/g, "imes")
       .replace(/a/g, "ai")
       .replace(/o/g, "ober")
       .replace(/u/g, "ufat");
 }
- 
+
+// Function to decrypt the string
 function decrypt(str) {
     str = str.toLowerCase();
+    // Replace the encryption keys with their respective letters
     return str.replace(/enter/g, "e")
       .replace(/imes/g, "i")
       .replace(/ai/g, "a")
@@ -22,14 +28,20 @@ function decrypt(str) {
       .replace(/ufat/g, "u");
 }
 
+// Add a click event listener to the encrypt button to run the encrypt function and update the output
 btnEncrypt.addEventListener("click", function() {
+     // Hide the empty container and show the full container
     emptyContainer.style.visibility = "hidden";
     fullContainer.style.visibility = "visible";
+    // Get the value of the input field
     const input = document.getElementById("input").value;
+     // Encrypt the input value
     const encrypted = encrypt(input);
+    // Update the output text with the encrypted string
     output.textContent = encrypted;
 });
 
+// Add a click event listener to the decrypt button to run the decrypt function and update the output
 btnDecrypt.addEventListener("click", function() {
     emptyContainer.style.visibility = "hidden";
     fullContainer.style.visibility = "visible";
